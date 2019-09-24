3d49b3c343fb3a65d6f32796438fbb3c138ced68
Diana <d7@hotmail.es>
2019-09-25 01:40:54 +0200

    Updates

---
+++ precache-manifest.fe81fdf214fac4edd0dd24387becb56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c5bb37be41908688f05747ca19852f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1fa78b8a45daa51eec55",
+    "url": "./static/css/main.daf10cbb.chunk.css"
+  },
+  {
+    "revision": "72bd03c7873e4da82e56",
+    "url": "./static/js/2.4cca9563.chunk.js"
+  },
+  {
+    "revision": "1fa78b8a45daa51eec55",
+    "url": "./static/js/main.d852f889.chunk.js"
+  },
+  {
+    "revision": "f49e0e74df6e6c3c28bc",
+    "url": "./static/js/runtime-main.04a13e19.js"
+  },
+  {
+    "revision": "d53af3ef2c06557f2b2650cda2dd5cc5",
+    "url": "./static/media/rules.d53af3ef.gif"
+  }
+]);
