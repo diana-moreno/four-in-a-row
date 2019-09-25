a2e1c1a38abe734a29f9846977a9c1f5bce8acba
Diana <d7@hotmail.es>
2019-09-25 18:34:30 +0200

    Updates

---
+++ precache-manifest.fbb9bfabac00178aabaa3f01fdd57a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9772c516b9cdd52fa6c0e5a32e05f1f1",
+    "url": "/four-in-a-row/index.html"
+  },
+  {
+    "revision": "63c083d8a3da7fd15be8",
+    "url": "/four-in-a-row/static/css/main.2ce23846.chunk.css"
+  },
+  {
+    "revision": "23acfe531657418020e0",
+    "url": "/four-in-a-row/static/js/2.c4b70f49.chunk.js"
+  },
+  {
+    "revision": "63c083d8a3da7fd15be8",
+    "url": "/four-in-a-row/static/js/main.8b54a456.chunk.js"
+  },
+  {
+    "revision": "5c3fb80f87d866cd7eb2",
+    "url": "/four-in-a-row/static/js/runtime-main.19ec890b.js"
+  },
+  {
+    "revision": "6d997f82b4986844d7c2f1b7cb700796",
+    "url": "/four-in-a-row/static/media/rules.6d997f82.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/four-in-a-row/precache-manifest.97c53e774de93ffeccdfd2f2a2b44a3b.js"
+  "/four-in-a-row/precache-manifest.fbb9bfabac00178aabaa3f01fdd57a2d.js"
 );
 
 self.addEventListener('message', (event) => {
